103d15b125731e119199d187499f11655095f8fd
zhou1112 <1142296909@qq.com>
2016-09-18 08:18:53 +0800

    bushu

--- src/About.js
+++ src/About.js
@@ -11,16 +11,24 @@ class About extends React.Component {
     }
   }
   componentDidMount(){
-    seachGit()
-    .then( (data) => {
-     console.log(data);
-     this.setState({
-      data:data.data,
-      wait:false
-    })
-    console.log(this.state.data);
-  });
+
 }
+ handleInput(e){
+   let value = e.target.value;
+   this.setState({inputValue:value})
+ }
+ handleClick(){
+   let name = this.state.inputValue;
+   seachGit(name)
+   .then( (data) => {
+    console.log(data);
+    this.setState({
+     data:data.data,
+     wait:false
+   })
+   console.log(this.state.data);
+ });
+ }
   render () {
     let gitInfo = (
         <div>
@@ -28,9 +36,21 @@ class About extends React.Component {
          <img src={this.state.data.avatar_url} />
         </div>
                 )
+    let styles= {
+      btn:{
+          width:'100px',
+          height:'30px',
+          backgroundColor:'purple',
+          fontSize:'18px',
+          textAlign:'center',
+          lineHeight:'30px'
+      }
+    }
   return(
         <div>
-         {
+          <input type="text" placeholder="uersname" value={this.state.inputValue} onChange={this.handleInput.bind(this)}/>
+          <button style={styles.btn} onClick={this.handleClick.bind(this)}>搜索</button><br />
+                   {
             this.state.wait ? '正在获取数据' :
             <img src={this.state.data.avatar_url} />
          }
--- uitll/helper.js
+++ uitll/helper.js
@@ -1,7 +1,10 @@
 
 import axios from 'axios';
-function seachGit(){
-  return axios.get('https://api.github.com/users/zhou1112')
+// function seachGit(){
+//   return axios.get('https://api.github.com/users/zhou1112')
+function seachGit (add) {
+   let address = `https://api.github.com/users/${add}`;
+   return axios.get(address)
   .then((res) =>
       ({data:res.data})
   )
@@ -10,3 +13,17 @@ function seachGit(){
   })
 }
 export {seachGit}
+
+
+function getJson() {
+   let address = `https://raw.githubusercontent.com/newming/demodata/master/card.json`;
+   return axios.get(address)
+     .then( (res) => (
+       { getJson:res.data }
+     ))
+     .catch(function (error) {
+       alert(error);
+     });
+ }
+
+ export { getJson };
