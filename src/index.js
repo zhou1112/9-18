ec3808e20017a7541399f6a51a6b9f5494875163
zhou1112 <1142296909@qq.com>
2016-09-19 01:39:45 +0800

    fz

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routers from './routes';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import './main.css';
-
-ReactDOM.render(<Routers />, document.getElementById('app'));
